dfbe8c84e9aa9ef4a3b3c60d8a6ec546c92bbba7
Crys <32685731+EugeneDvorkin@users.noreply.github.com>
2018-03-17 00:02:38 +0300

    Day2-3
    Task6 with tests(without DDT)

---
+++ NET.S.2018.Dvorkin.02/Task6.NUnitTests/FilterNUnitTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace Task6.NUnitTests
+{
+    [TestFixture]
+    public class FilterNUnitTests
+    {
+        static int[] arrayForFilter = { 1, 2, 22, 232, 5, 55, 222, 8, 9, 123 };
+        static int[] filtredArray = { 2, 22, 232, 222, 123 };
+        static int[] arrayForFilterWithNegat = { 1, -2, -22, -232, 5, 55, -222, 8, 9, -123 };
+        static int[] filtredArrayWithNegate = { -2, -22, -232, -222, -123 };
+
+        static object[] ListForSort =
+        {
+            new object[]{ arrayForFilter, filtredArray },
+            new object[]{ arrayForFilterWithNegat, filtredArrayWithNegate }
+        };
+
+        [Test, TestCaseSource("ListForSort")]
+        public void FilterDigit_ArrayFor10Elem_FiltredArray(int[] arrayForFilter, int[] filtredArray)
+        {
+
+            int[] actualArray = Filter.FilterDigit(arrayForFilter, 2);
+
+            CollectionAssert.AreEqual(filtredArray, actualArray);
+        }
+
+        [Test, TestCase(new int[] { 1, 0, 10, 232, 5, 100, 222, 8, 1, 123 }, new int[] { 0, 10, 100 }, 0)]        
+        public void FilterDigit_ArraySomeIntElem_FiltredArray(int[] arrayForFilter, int[] filtredArray, int flag)
+        {
+
+            int[] actualArray = Filter.FilterDigit(arrayForFilter, flag);
+
+            CollectionAssert.AreEqual(filtredArray, actualArray);
+        }
+
+        [Test]
+        public void FilterDigit_ArrayArgumentNullException_ArgumentNullException()
+        {
+            Assert.That(() => Filter.FilterDigit(null, 9), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Filter_DigirMoreNine_ArgumentException()
+        {
+            Assert.That(() => Filter.FilterDigit(new int[] { 1, 1, 2, 3 }, 11), Throws.ArgumentException);
+        }        
+    }
+}
---
+++ NET.S.2018.Dvorkin.02/Task6.Tests/FilterTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Task6.Tests
+{
+    [TestClass]
+    public class FilterTests
+    {
+        [TestMethod]
+        public void FilterDigit_ArrayFor10Elem_FiltredArray()
+        {
+            int[] arrayForFilter = { 1, 2, 22, 232, 5, 55, 222, 8, 9, 123 };
+            int[] filtredArray = { 2, 22, 232, 222, 123 };
+
+            int[] actualArray = Filter.FilterDigit(arrayForFilter, 2);
+
+            CollectionAssert.AreEqual(filtredArray, actualArray);
+        }
+
+        [TestMethod]
+        public void FilterDigit_ArrayWithNegatElem_FiltredArray()
+        {
+            int[] arrayForFilter = { 1, -2, -22, -232, 5, 55, -222, 8, 9, -123 };
+            int[] filtredArray = { -2, -22, -232, -222, -123 };
+
+            int[] actualArray = Filter.FilterDigit(arrayForFilter, 2);
+
+            CollectionAssert.AreEqual(filtredArray, actualArray);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FilterDigir_DigitIsNotADigit_ArgumentOutOfRangeException()
+        {
+            int[] arrayForFilter = { 1, -2, -22, -232, 5, 55, -222, 8, 9, -123 };
+            int[] filtredArray = { -2, -22, -232, -222, -123 };
+
+            int[] actualArray = Filter.FilterDigit(arrayForFilter, 10);
+
+            CollectionAssert.AreEqual(filtredArray, actualArray);
+        }        
+    }
+}
