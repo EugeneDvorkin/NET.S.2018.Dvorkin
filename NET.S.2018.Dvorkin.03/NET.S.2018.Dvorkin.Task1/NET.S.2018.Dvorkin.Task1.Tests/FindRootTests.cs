c8ec749aef2be165ac05335db8e057eaeb17fb3a
Eugene <crusheider@gmail.com>
2018-03-19 17:57:49 +0300

    Task1
    Task1 of day3

---
+++ NET.S.2018.Dvorkin.03/NET.S.2018.Dvorkin.Task1/NET.S.2018.Dvorkin.Task1.Tests/FindRootTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NET.S._2018.Dvorkin.Task1.Tests
+{
+    [TestFixture]
+    public class FindRootTests
+    {
+        [TestCase(1, 5, 0.0001)]
+        public void FindRoots_1_5_00001_Expect1(double element, int pow, double eps) => Assert.AreEqual(1, FindRoot.FindNthRoot(element, pow, eps), 0.0001);
+
+        [TestCase(8, 3, 0.0001)]
+        public void FindRoots_8_3_00001_Expect2(double element, int pow, double eps)
+        {
+            Assert.AreEqual(2, FindRoot.FindNthRoot(element, pow, eps), 0.0001);
+        }
+
+        [TestCase(0.001, 3, 0.0001)]
+        public void FindRoots_0001_3_00001_Expect01(double element, int pow, double eps)
+        {
+            Assert.AreEqual(0.1, FindRoot.FindNthRoot(element, pow, eps), 0.0001);
+        }
+
+        [TestCase(0.04100625, 4, 0.0001)]
+        public void FindRoots_004100625_4_00001_Expect045(double element, int pow, double eps)
+        {
+            Assert.AreEqual(0.45, FindRoot.FindNthRoot(element, pow, eps), 0.0001);
+        }
+        
+        [TestCase(0.0279936, 7, 0.0001)]
+        public void FindRoots_00279936_7_00001_Expect06(double element, int pow, double eps)
+        {
+            Assert.AreEqual(0.6, FindRoot.FindNthRoot(element, pow, eps), 0.0001);
+        }
+
+        [TestCase(0.0081, 4, 0.1)]
+        public void FindRoots_00081_4_01_Expect03(double element, int pow, double eps)
+        {
+            Assert.AreEqual(0.3, FindRoot.FindNthRoot(element, pow, eps), 0.1);
+        }
+
+        [TestCase(-0.008, 3, 0.1)]
+        public void FindRoots_M0008_3_01_ExpectM02(double element, int pow, double eps)
+        {
+            Assert.AreEqual(-0.2, FindRoot.FindNthRoot(element, pow, eps), 0.1);
+        }
+
+        [TestCase(0.004241979, 9, 0.00000001)]
+        public void FindRoots_0004241979_9_000000001_Expect0545(double element, int pow, double eps)
+        {
+            Assert.AreEqual(0.545, FindRoot.FindNthRoot(element, pow, eps), 0.00000001);
+        }
+
+        [TestCase(8, 15, -7)]
+        public void FindRoots_8_15_M7_ArgumentException(double element, int pow, double eps)
+        {
+            Assert.Throws<ArgumentException>(() => FindRoot.FindNthRoot(element, pow, eps));
+        }
+
+        [TestCase(8, 15, -0.6)]
+        public void FindRoots_8_15_M06_ArgumentException(double element, int pow, double eps)
+        {
+            Assert.Throws<ArgumentException>(() => FindRoot.FindNthRoot(element, pow, eps));
+        }
+    }
+}
---
+++ NET.S.2018.Dvorkin.03/NET.S.2018.Dvorkin.Task1/NET.S.2018.Dvorkin.Task1/FindRoot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace NET.S._2018.Dvorkin.Task1
+{
+    /// <summary>
+    /// Contains function for Sqrt by Newton method
+    /// </summary>
+    public class FindRoot
+    {
+        /// <summary>
+        /// Sqrt by Newton method
+        /// </summary>
+        /// <param name="element">number for Sqrt</param>
+        /// <param name="pow">Degree for Sqrt</param>
+        /// <param name="eps">Precision</param>
+        /// <returns>Result of Sqrt</returns>
+        public static double FindNthRoot(double element, int pow, double eps)
+        {
+            if (pow < 0)
+            {
+                throw new ArgumentException($"{nameof(pow)} must be more than 0");
+            }
+
+            if (element < 0 && pow % 2 == 0)
+            {
+                throw new ArgumentException($"{nameof(element)} and {nameof(pow)} can't be is less, than 0 and be even at the same time");
+            }
+
+            if (eps < 0)
+            {
+                throw new ArgumentException($"{nameof(eps)} is less than 0");
+            }
+
+            double x0 = pow;
+            double x1 = element / eps;
+
+            while (Math.Abs(x1 - x0) > eps)
+            {
+                x0 = x1;
+                x1 = 1.0 / element * ((element - 1) * x1 + element / Math.Pow(x1, pow - 1));
+            }
+
+            return x1;
+        }
+    }
+}
