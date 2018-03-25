11bcdc2e672c32d6beb309c931d050d0cc35f0ad
Crys <crusheider@gmail.com>
2018-03-25 22:22:38 +0300

    Task 1

---
+++ NET.S.2018.Dvorkin.06/NET.S.2018.Dvorkin.Task2/NET.S.2018.Dvorkin.Task2.Tests/PolynomialTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NET.S._2018.Dvorkin.Task2.Tests
+{
+    [TestFixture]
+    public class PolynomialTests
+    {
+        [TestCase("1 2 3 4 5 6 7 ", 1, 2, 3, 4, 5, 6, 7)]
+        [TestCase("1,5 2,5 3,5 4,5 5,5 6,5 7,5 ", 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5)]
+        public void PolynomialToString_ValidParams_ValidString(string expected, params double[] test)
+        {
+            Polynomial polynomial = new Polynomial(test);
+            Assert.AreEqual(expected, polynomial.ToString());
+        }
+
+        [TestCase()]
+        public void PolynomialToString_InvalidParams_ArgumentExeptions(params double[] test)
+        {
+            Polynomial polynomial;
+            Assert.Throws<ArgumentException>(() => polynomial = new Polynomial(test));
+        }
+
+        [TestCase("2 4 6 ")]
+        public void PolynomialAdd_validTwoPolynomial_ValidResult(string expected)
+        {
+            Polynomial poly1 = new Polynomial(1, 2, 3);
+            Polynomial poly2 = new Polynomial(1, 2, 3);
+            Polynomial result = Polynomial.Add(poly1, poly2);
+            string res = result.ToString();
+            Assert.AreEqual(expected, res);        
+        }
+
+        [TestCase]
+        public void PolynomialAdd_ValidParams_ValidResult()
+        {
+            Polynomial poly1 = new Polynomial(2, 4, 6);
+            Polynomial poly2 = new Polynomial(1, 2, 3);
+            Polynomial result = poly1 - poly2;
+            string res = result.ToString();
+            string expected = "1 2 3 ";
+            Assert.AreEqual(expected, res);
+        }
+    }
+}
---
+++ NET.S.2018.Dvorkin.06/NET.S.2018.Dvorkin.Task2/NET.S.2018.Dvorkin.Task2/Polynomial.cs
@@ -0,0 +1,488 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace NET.S._2018.Dvorkin.Task2
+{
+    /// <summary>
+    /// Contains methods for polynomials
+    /// </summary>
+    /// <seealso cref="System.Collections.IEnumerable" />
+    /// <seealso cref="System.IComparable" />
+    public class Polynomial : IEnumerable, IComparable
+    {
+        /// <summary>
+        /// The collection of coefficients
+        /// </summary>
+        private readonly double[] coeff;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Polynomial"/> class.
+        /// </summary>
+        /// <param name="coeff">The coefficients.</param>
+        /// <exception cref="ArgumentNullException"> coefficients</exception>
+        /// <exception cref="ArgumentException"> coefficients</exception>
+        public Polynomial(params double[] coeff)
+        {
+            if (coeff == null)
+            {
+                throw new ArgumentNullException($"{nameof(coeff)} is null");
+            }
+
+            if (coeff.Length == 0)
+            {
+                throw new ArgumentException($"{nameof(coeff)} length is 0");
+            }
+
+            this.coeff = new double[coeff.Length];
+            coeff.CopyTo(this.coeff, 0);
+        }
+
+        /// <summary>
+        /// Gets the length.
+        /// </summary>
+        /// <value>
+        /// The length.
+        /// </value>
+        public int Length
+        {
+            get { return this.coeff.Length; }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="System.Double"/> with the specified indexer.
+        /// </summary>
+        /// <value>
+        /// The <see cref="System.Double"/>.
+        /// </value>
+        /// <param name="indexer">The indexer.</param>
+        /// <returns>Index of current element</returns>
+        /// <exception cref="ArgumentException">indexer</exception>
+        public double this[int indexer]
+        {
+            get
+            {
+                if (indexer < 0 || indexer > this.coeff.Length)
+                {
+                    throw new ArgumentException($"{nameof(indexer)} is invalid");
+                }
+
+                return this.coeff[indexer];
+            }
+        }
+
+        /// <summary>
+        /// Equals the specified polynomials.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        /// <returns>
+        /// <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool Equals(Polynomial poly1, Polynomial poly2)
+        {
+            if (poly1.Equals(poly2) == true)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Implements the operator +.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Polynomial operator +(Polynomial poly1, Polynomial poly2) => Sum(poly1, poly2);
+
+        /// <summary>
+        /// Implements the operator +.
+        /// </summary>
+        /// <param name="poly">The polynomial.</param>
+        /// <param name="number">The number.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Polynomial operator +(Polynomial poly, double number) => Sum(poly, number);
+
+        /// <summary>
+        /// Implements the operator -.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Polynomial operator -(Polynomial poly1, Polynomial poly2) => Subtr(poly1, poly2);
+
+        /// <summary>
+        /// Implements the operator -.
+        /// </summary>
+        /// <param name="poly">The polynomial.</param>
+        /// <param name="number">The number.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Polynomial operator -(Polynomial poly, double number) => Subtr(poly, number);
+
+        /// <summary>
+        /// Implements the operator *.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Polynomial operator *(Polynomial poly1, Polynomial poly2) => Mult(poly1, poly2);
+
+        /// <summary>
+        /// Implements the operator *.
+        /// </summary>
+        /// <param name="poly">The polynomial.</param>
+        /// <param name="number">The number.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Polynomial operator *(Polynomial poly, double number) => Mult(poly, number);
+
+        /// <summary>
+        /// Implements the operator ==.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator ==(Polynomial poly1, Polynomial poly2) => Equals(poly1, poly2);
+
+        /// <summary>
+        /// Implements the operator !=.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator !=(Polynomial poly1, Polynomial poly2) => !Equals(poly1, poly2);
+
+        /// <summary>
+        /// Adds the specified polynomial 1 to polynomial 2.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        /// <returns>Sum of polynomial 1 and polynomial 2.</returns>
+        public static Polynomial Add(Polynomial poly1, Polynomial poly2) => Sum(poly1, poly2);
+
+        /// <summary>
+        /// Adds the specified polynomial 1 to number.
+        /// </summary>
+        /// <param name="poly">The polynomial.</param>
+        /// <param name="number">The number.</param>
+        /// <returns>Sum of polynomial and number.</returns>
+        public static Polynomial Add(Polynomial poly, double number) => Sum(poly, number);
+
+        /// <summary>
+        /// Subtracts the specified polynomial 1 to polynomial 2.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        /// <returns>Subtract of polynomial 1 and polynomial 2.</returns>
+        public static Polynomial Subtract(Polynomial poly1, Polynomial poly2) => Subtr(poly1, poly2);
+
+        /// <summary>
+        /// Subtracts the specified polynomial to number.
+        /// </summary>
+        /// <param name="poly">The to number.</param>
+        /// <param name="number">The number.</param>
+        /// <returns>Subtract of polynomial and number.</returns>
+        public static Polynomial Subtract(Polynomial poly, double number) => Subtr(poly, number);
+
+        /// <summary>
+        /// Multiplies the specified polynomial 1 to polynomial 2.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        /// <returns>Multiplies of polynomial 1 and polynomial 2.</returns>
+        public static Polynomial Multiply(Polynomial poly1, Polynomial poly2) => Mult(poly1, poly2);
+
+        /// <summary>
+        /// Multiplies the specified polynomial to number.
+        /// </summary>
+        /// <param name="poly">The polynomial.</param>
+        /// <param name="number">The number.</param>
+        /// <returns>Multiplies of polynomial and number.</returns>
+        public static Polynomial Multiply(Polynomial poly, double number) => Mult(poly, number);
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator GetEnumerator()
+        {
+            return this.coeff.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Compares the current instance with another object of the same type and returns an integer that indicates whether the current instance precedes, follows, or occurs in the same position in the sort order as the other object.
+        /// </summary>
+        /// <param name="obj">An object to compare with this instance.</param>
+        /// <returns>
+        /// A value that indicates the relative order of the objects being compared. 
+        /// The return value has these meanings: Value Meaning Less than zero This instance precedes <paramref name="obj" /> in the sort order. 
+        /// Zero This instance occurs in the same position in the sort order as <paramref name="obj" />. 
+        /// Greater than zero This instance follows <paramref name="obj" /> in the sort order.
+        /// </returns>
+        public int CompareTo(object obj)
+        {
+            Polynomial poly = obj as Polynomial;
+            Polynomial.Check(poly);
+            double[] longest = (poly.Coeff().Length > this.Coeff().Length) ? poly.Coeff() : this.Coeff();
+            poly.Coeff().Reverse();
+            this.Coeff().Reverse();
+            for (int i = 0; i < longest.Length; i++)
+            {
+                if (poly[i] > this[i])
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Get current coefficients of the polynomial.
+        /// </summary>
+        /// <returns>Coefficients.</returns>
+        public double[] Coeff()
+        {
+            double[] coeff = new double[this.coeff.Length];
+            this.coeff.CopyTo(coeff, 0);
+
+            return coeff;
+        }
+       
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            Polynomial poly = obj as Polynomial;
+
+            if (poly == null)
+            {
+                return false;
+            }
+
+            if (poly.Coeff().Length != this.Coeff().Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < poly.Coeff().Length; i++)
+            {
+                if (poly.Coeff()[i] != this.Coeff()[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            string result = string.Empty;
+            foreach (var item in this.coeff)
+            {
+                result += item + " ";
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Sums the specified polynomial 1 to polynomial 2.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        /// <returns>Sum of polynomial 1 and polynomial 2.</returns>
+        private static Polynomial Sum(Polynomial poly1, Polynomial poly2)
+        {
+            Polynomial.Check(poly1, poly2);
+            double[] longest = (poly1.Coeff().Length > poly2.Coeff().Length) ? poly1.Coeff() : poly2.Coeff();
+            double[] shortest = (poly1.Coeff() == longest) ? poly2.Coeff() : poly1.Coeff();
+            double[] result = new double[longest.Length];
+            longest.CopyTo(result, 0);
+            for (int i = 0; i < shortest.Length; i++)
+            {
+                result[i] += shortest[i];
+            }
+
+            return new Polynomial(result);
+        }
+
+        /// <summary>
+        /// Sums the specified polynomial 1 to number..
+        /// </summary>
+        /// <param name="poly">The polynomial.</param>
+        /// <param name="number">The number.</param>
+        /// <returns>Sums of polynomial and number.</returns>
+        private static Polynomial Sum(Polynomial poly, double number)
+        {
+            Polynomial.Check(poly);
+            double[] result = new double[poly.Coeff().Length];
+            poly.Coeff().CopyTo(result, 0);
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] += number;
+            }
+
+            return new Polynomial(result);
+        }
+
+        /// <summary>
+        /// Subtracts the specified polynomial 1 to polynomial 2.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        /// <returns>Subtract of polynomial 1 and polynomial 2.</returns>
+        private static Polynomial Subtr(Polynomial poly1, Polynomial poly2)
+        {
+            Polynomial.Check(poly1, poly2);
+            double[] longest = (poly1.Coeff().Length > poly2.Coeff().Length) ? poly1.Coeff() : poly2.Coeff();
+
+            double[] result = new double[longest.Length];
+            poly1.Coeff().CopyTo(result, 0);
+
+            for (int i = 0; i < poly2.Coeff().Length; i++)
+            {
+                result[i] -= poly2[i];
+            }
+
+            return new Polynomial(result);
+        }
+
+        /// <summary>
+        /// Subtracts the specified polynomial to number.
+        /// </summary>
+        /// <param name="poly">The polynomial.</param>
+        /// <param name="number">The number.</param>
+        /// <returns>Subtracts of polynomial and number.</returns>
+        private static Polynomial Subtr(Polynomial poly, double number)
+        {
+            Polynomial.Check(poly);
+            double[] result = new double[poly.Coeff().Length];
+            poly.Coeff().CopyTo(result, 0);
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] -= number;
+            }
+
+            return new Polynomial(result);
+        }
+
+        /// <summary>
+        /// Multiplies the specified polynomial 1 to polynomial 2.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        /// <returns>Multiplies of polynomial 1 and polynomial 2.</returns>
+        private static Polynomial Mult(Polynomial poly1, Polynomial poly2)
+        {
+            Polynomial.Check(poly1, poly2);
+            double[] longest = (poly1.Coeff().Length > poly2.Coeff().Length) ? poly1.Coeff() : poly2.Coeff();
+            double[] result = new double[longest.Length];
+            poly1.Coeff().CopyTo(result, 0);
+            for (int j = 0; j < poly1.Length; j++)
+            {
+                for (int i = 0; i < poly2.Length; i++)
+                {
+                    result[j + i] += poly2[i] + poly1[j];
+                }
+            }
+
+            return new Polynomial(result);
+        }
+
+        /// <summary>
+        /// Multiplies the specified polynomial to number.
+        /// </summary>
+        /// <param name="poly">The polynomial.</param>
+        /// <param name="number">The number.</param>
+        /// <returns>Multiplies of polynomial and number.</returns>
+        private static Polynomial Mult(Polynomial poly, double number)
+        {
+            Polynomial.Check(poly);
+            double[] result = new double[poly.Coeff().Length];
+            poly.Coeff().CopyTo(result, 0);
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] *= number;
+            }
+
+            return new Polynomial(result);
+        }
+
+        /// <summary>
+        /// Validation of polynomial 1 and polynomial 2.
+        /// </summary>
+        /// <param name="poly1">The polynomial 1.</param>
+        /// <param name="poly2">The polynomial 2.</param>
+        private static void Check(Polynomial poly1, Polynomial poly2)
+        {
+            Check(poly1);
+            Check(poly2);
+        }
+
+        /// <summary>
+        /// Validation of polynomial.
+        /// </summary>
+        /// <param name="poly">The polynomial.</param>
+        /// <exception cref="ArgumentNullException">polynomial</exception>
+        /// <exception cref="ArgumentException">polynomial</exception>
+        private static void Check(Polynomial poly)
+        {
+            if (poly == null)
+            {
+                throw new ArgumentNullException($"{nameof(poly)} is null");
+            }
+
+            if (poly.Coeff().Length == 0)
+            {
+                throw new ArgumentException($"{nameof(poly)} has 0 arguments");
+            }
+        }
+    }
+}
