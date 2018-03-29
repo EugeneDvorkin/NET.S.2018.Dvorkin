812890743a3428b3f61cb81993ecf0c7aedec80e
Eugene <crusheider@gmail.com>
2018-03-29 13:57:12 +0300

    Task
    Added task

---
+++ NET.S.2018.Dvorkin.08/NET.S.2018.Dvorkin.Task1/NET.S.2018.Dvorkin.Task1/IBankService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace NET.S._2018.Dvorkin.Task1
+{
+    /// <summary>
+    /// Contains required methods for bank account.
+    /// </summary>
+    /// <typeparam name="T">Type of account.</typeparam>
+    interface IBankService<T>
+    {
+        T Replenishment(decimal account, T acc);
+        T Withdrawal(decimal account, T acc);
+        //T NewAcc(string number, string name, string surname, string passport, decimal account = 0, int score = 0);
+        void CloseAcc(T acc);
+    }
+}
