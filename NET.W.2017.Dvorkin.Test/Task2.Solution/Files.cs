95586a2542dd90bd3fa5ddff896bb8fe5f0d1435
Crys <32685731+EugeneDvorkin@users.noreply.github.com>
2018-04-23 11:28:04 +0300

    Edit

--- NET.W.2017.Dvorkin.Test/Task2.Solution/Files.cs
+++ NET.W.2017.Dvorkin.Test/Task2.Solution/Files.cs
@@ -8,7 +8,7 @@ namespace Task_2
         public abstract string WorkingDirectory { get; }
         public abstract string FileExtension { get; }
 
-        private void WriteBytesToFile(string WorkingDirectory,string fileName, byte[] content)
+        private void WriteBytesToFile(string fileName, byte[] content)
         {
             if (!Directory.Exists(WorkingDirectory))
             {
@@ -26,7 +26,7 @@ namespace Task_2
 
                 var generatedFileName = $"{Guid.NewGuid()}{FileExtension}";
 
-                WriteBytesToFile(WorkingDirectory, generatedFileName, generatedFileContent);
+                WriteBytesToFile(generatedFileName, generatedFileContent);
             }
         }
 
