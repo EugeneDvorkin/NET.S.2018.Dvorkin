8610448aee6ce8ff0decbb821286dd2b0365641c
Eugene <crusheider@gmail.com>
2018-04-26 15:26:59 +0300

    Task5

---
+++ NET.W.2017.Dvorkin.Test/Task5.Solution/ConcretVisitor.cs
@@ -0,0 +1,22 @@
+﻿namespace Task5.Solution
+{
+    public class ConcretVisitor:Visitor
+    {
+        public string ConcretString { get; private set; }
+
+        protected override void Visit(BoldText boldText)
+        {
+            ConcretString = boldText.Text;
+        }
+
+        protected override void Visit(Hyperlink hyperlink)
+        {
+            ConcretString = hyperlink.Url + hyperlink.Text;
+        }
+
+        protected override void Visit(PlainText plainText)
+        {
+            ConcretString = plainText.Text;
+        }
+    }
+}
---
+++ NET.W.2017.Dvorkin.Test/Task5.Solution/Visitor.cs
@@ -0,0 +1,12 @@
+﻿namespace Task5.Solution
+{
+    public abstract class Visitor
+    {
+        public void TypeVisit(DocumentPart documentPart) => Visit((dynamic)documentPart);
+        protected abstract void Visit(BoldText boldText);
+
+        protected abstract void Visit(Hyperlink hyperlink);
+
+        protected abstract void Visit(PlainText plainText);
+    }
+}
